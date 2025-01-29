46a406af3c1426112028ae4a8f0a5363155510fd
Luis M <manjarrez02@gmail.com>
2025-01-29 16:41:41 -0600

    Adds invoice generation for e-wallet recharge

--- custom_addon/pos_customer_wallet_management/wizard/recharge_wallet.py
+++ custom_addon/pos_customer_wallet_management/wizard/recharge_wallet.py
@@ -28,7 +28,8 @@ class RechargeWallet(models.TransientModel):
     _description = "Create Wallet Recharge Of Each Customer"
 
     journal_id = fields.Many2one("account.journal", string="Payment Journal",
-                                 help="Select journal type", domain=[("type", "in", ["cash", "bank"]), ("wallet_journal", "=", False)])
+                                 help="Select journal type", domain=[("type", "in", ["cash", "bank"]), ("wallet_journal", "=", False)],
+                                default=lambda self: self.env["account.journal"].search([("type", "=", "cash")], limit=1).id)
     recharge_amount = fields.Float(string="Recharge Amount",
                                    help="Recharge amount in wallet")
 
@@ -52,6 +53,37 @@ class RechargeWallet(models.TransientModel):
             'journal_id': self.journal_id.id
         })
         pay_rec.action_post()
+        move_vals = {
+            'partner_id': partner.id,  # Cliente al que se le factura
+            'move_type': 'out_invoice',  # Factura de cliente
+            'invoice_date': fields.Date.today(),  # Fecha de la factura
+            'invoice_line_ids': [(0, 0, {
+                'name': 'Recarga de e-wallet',  # Etiqueta en la línea
+                'quantity': 1,  # Cantidad fija
+                'price_unit': self.recharge_amount,  # Monto del pago (ajusta según necesidad)                
+            })]
+        }
+        
+        invoice = self.env['account.move'].sudo().with_context(default_move_type=move_vals['move_type']).create(move_vals)
+        invoice.action_post()
+        # Reconciliar la factura con el pago
+        receivable_account = self.env["res.partner"]._find_accounting_partner(partner).property_account_receivable_id
+
+        if receivable_account.reconcile:
+            # Obtener las líneas contables de la factura que no están reconciliadas
+            invoice_receivables = invoice.line_ids.filtered(
+                lambda line: line.account_id == receivable_account and not line.reconciled
+            )
+
+            if invoice_receivables:
+                # Obtener las líneas de pago
+                payment_receivables = pay_rec.move_id.line_ids.filtered(
+                    lambda line: line.account_id == receivable_account and line.partner_id == partner
+                )
+
+                # Reconciliar las líneas de la factura y del pago
+                (invoice_receivables | payment_receivables).sudo().reconcile()
+
 
     @api.model
     def frontend_recharge(self, partner, amount_input, currency):
