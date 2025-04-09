f4660ac31af52c7f5d402574772ceeb56a3df207
Luis M <manjarrez02@gmail.com>
2025-04-09 00:40:05 -0600

    Fixes break time order

--- custom_addon/hr_zk_attendance/models/zk_machine.py
+++ custom_addon/hr_zk_attendance/models/zk_machine.py
@@ -197,7 +197,7 @@ class ZkMachine(models.Model):
                                             if each.punch in [5, 15]: #break-out
                                                 if len(att_var) == 1:
                                                     if att_var.break_out:
-                                                       if att_var.break_out < atten_time:
+                                                       if fields.Datetime.to_string(att_var.break_out) < atten_time:
                                                             att_var.write({'break_in': atten_time})
                                                        else:
                                                            att_var.write({'break_in': att_var.break_out})
