006d6e754b3847fca4c993c5b68ac2f4b01cd0c1
Luis M <manjarrez02@gmail.com>
2025-05-05 15:52:06 -0600

    Fixes problems with e-wallet when applies invoice payment and customer screen opening

--- custom_addon/pos_customer_screen/controllers/main.py
+++ custom_addon/pos_customer_screen/controllers/main.py
@@ -12,7 +12,8 @@
 from odoo import http
 from odoo.http import request
 from odoo.addons.bus.controllers.main import BusController
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class CustomerDisplayController(BusController):
     def _poll(self, dbname, channels, last, options):
@@ -34,10 +35,9 @@ class PosMirrorController(http.Controller):
     @http.route('/web/customer_display', type='http', auth='user')
     def white_board_web_1(self, **k):
         config_id = False
-        pos_sessions = request.env['pos.session'].search([
-            ('state', '=', 'opened'),
-            ('user_id', '=', request.session.uid),
-            ('rescue', '=', False)])
+        pos_sessions = request.env['pos.session'].sudo().search([
+            ('state', '=', 'opened'),            
+            ('rescue', '=', False)]) #Se elimina la condición ('user_id', '=', request.session.uid) para uso de multiples usuarios 
         if pos_sessions:
             for pos_session in pos_sessions:
                 if pos_session.config_id.id == int(k.get('config_id')):
@@ -47,6 +47,10 @@ class PosMirrorController(http.Controller):
             'session_info': session_info,
             'config_id': config_id,
         }
+
+        _logger.debug("[CUSTOMER DISPLAY] Sesiones POS encontradas: %s", pos_sessions)
+        _logger.debug("[CUSTOMER DISPLAY] Contexto enviado al render: %s", context)
+
         return request.render('pos_customer_screen.index', qcontext=context)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- custom_addon/pos_customer_wallet_management/static/src/js/CustomRegisterInvoicePaymentPopupWidget.js
+++ custom_addon/pos_customer_wallet_management/static/src/js/CustomRegisterInvoicePaymentPopupWidget.js
@@ -3,6 +3,9 @@ odoo.define('pos_customer_wallet_management.CustomRegisterInvoicePaymentPopupWid
 
     const RegisterInvoicePaymentPopupWidget = require('pos_all_in_one.RegisterInvoicePaymentPopupWidget');
     const Registries = require('point_of_sale.Registries');
+    const rpc = require('web.rpc');
+	let core = require('web.core');
+	let _t = core._t;
 
     const CustomRegisterInvoicePaymentPopupWidget = (RegisterInvoicePaymentPopupWidget) =>
         class extends RegisterInvoicePaymentPopupWidget {
@@ -11,43 +14,60 @@ odoo.define('pos_customer_wallet_management.CustomRegisterInvoicePaymentPopupWid
                 var invoice = this.invoice;
                 var partner = invoice.partner_id[0];
                 var payment_type = $('#payment_type1').val();
-
-                var entered_amount = parseFloat(($("#entered_amount1").val()).replace(/,/g, ""));
+                var entered_amount = ($("#entered_amount1").val()).replace(",", "");
                 var entered_note = $("#entered_note1").val();
-                let rpc_result = false;
+                var rpc_result = false;
 
-                /**
-                 *  Verificar saldo de wallet utilizado
-                 */
+                var journal = this.env.pos.journals.find(({ id }) => id === Number(payment_type));
+                var partner_ = this.env.pos.partners.find(({ id }) => id === Number(partner));
 
-                var totalWalletPayment = entered_amount;
-                var wallet_balance = this.env.pos.selectedOrder.partner.wallet_balance;
-                let wallet_balance_rounded = parseFloat(wallet_balance.toFixed(2));
-                let totalWalletPayment_rounded = parseFloat(totalWalletPayment.toFixed(2));                            
-                
-                if (!isNaN(entered_amount)){
+                if (isNaN(entered_amount)) {
                     await this.showPopup('ErrorPopup', {
                         title: this.env._t('Unknown'),
-                        body: "Ingrese un valor válido.",
+                        body: "Ingrese valor válido.",
                     });
-                    return; 
+                    return;   
                 }
 
+                if(journal.wallet_journal){                
+                    var wallet_balance = partner_.wallet_balance;
+                    // Verificar si el total de los pagos supera el balance de la billetera
+                    if (wallet_balance < entered_amount) {
+                        await this.showPopup('ErrorPopup', {
+                            title: this.env._t('Saldo insuficiente en la e-wallet.'),
+                            body: "Saldo insuficiente en la e-wallet.",
+                        });
+                        return;                
+                    }            
+                }                
+                
+                if (invoice['amount_residual'] >= entered_amount){
+                    var session_id = this.env.pos.config.current_session_id[0];
+                    var default_customer = this.env.pos.config.res_partner_id;
+	                var default_customer_by_id = this.env.pos.db.get_partner_by_id(default_customer[0]);
+                    if(default_customer_by_id){
+                        this.env.pos.get_order().set_partner(default_customer_by_id);
+                    } else{
+                        this.env.pos.get_order().set_partner(null);
+                    }
 
-                // Verificar si el total de los pagos supera el balance de la billetera
-                if (wallet_balance_rounded < totalWalletPayment_rounded) {   
-                    await this.showPopup('ErrorPopup', {
-                        title: this.env._t('Saldo Insuficiente'),
-                        body: "Saldo insuficiente en la e-wallet",
-                    });
-                    return; // Detener la validación si el balance es insuficiente
-                }
-    
-                if (invoice['amount_residual'] > entered_amount){
+                    if(journal.wallet_journal){
+                        var price = parseFloat(entered_amount);                        
+                        var session = this.env.pos.config.current_session_id[1];
+                        var currency_id = this.env.pos.company.currency_id[1];
+                        var order_partner = partner_;
+                        var order_name = invoice['name'];
+                        var balance = parseFloat(wallet_balance - price);                                                                   
+                        rpc.query({
+                            model: 'res.partner',
+                            method: 'write_value',
+                            args: [balance, order_partner, order_name, price, currency_id],
+                        });
+                    }
                     rpc_result = rpc.query({
                         model: 'pos.create.customer.payment',
                         method: 'create_customer_payment_inv',
-                        args: [partner ? partner : 0, partner ? partner : 0, payment_type, entered_amount, invoice, entered_note],
+                        args: [partner ? partner : 0, partner ? partner : 0, payment_type, entered_amount, invoice, entered_note,session_id],
                         
                     }).then(function(output) {
                          rpc.query({
@@ -55,7 +75,7 @@ odoo.define('pos_customer_wallet_management.CustomRegisterInvoicePaymentPopupWid
                             method: 'update_partner_credit_amount',
                             args: [partner, entered_amount],
                         });
-                        alert('Payment has been Registered for this Invoice !!!!');
+                        
                         self.cancel()
                         self.showTempScreen('RegisterInvoicePaymentRecipetScreen',{
                             partner_id : output.partner,
@@ -69,12 +89,14 @@ odoo.define('pos_customer_wallet_management.CustomRegisterInvoicePaymentPopupWid
                     });
     
                     
-                }else{
+                }                
+                else{
                     self.showPopup('ErrorPopup', {
                         'title': _t('Amount Error'),
                         'body': _t('Entered amount is larger then due amount. please enter valid amount'),
                     });
-                }    
+                }
+    
             }
         };
 
